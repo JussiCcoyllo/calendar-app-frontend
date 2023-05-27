913ab3e134df0b5f6d848f8a114ab2d0a2701cb7
Alexey Li <aleksey.lee@gmail.com>
2023-05-27 18:20:13 +0200

    add login-registration-Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { HttpClientModule } from "@angular/common/http"
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainPageComponent } from './main-page/main-page.component';
+import { LoginComponent } from './main-page/login/login.component';
+import { LoginWithPasswordComponent } from './main-page/login/login-with-password/login-with-password.component';
+import { RegisterComponent } from './main-page/register/register.component';
+import { RegisterWithPasswordComponent } from './main-page/register/register-with-password/register-with-password.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatPseudoCheckboxModule } from '@angular/material/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTabsModule } from '@angular/material/tabs';
 
 
 @NgModule({
-  declarations: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  declarations: [
+    AppComponent, 
+    MainPageComponent, 
+    LoginComponent, 
+    LoginWithPasswordComponent, 
+    RegisterComponent, 
+    RegisterWithPasswordComponent
+    
+  ],
   imports: [
     BrowserModule,
     FullCalendarModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatPseudoCheckboxModule,
+    MatSnackBarModule,
+    MatTabsModule
+    
+
 
   ],
   providers: [],
