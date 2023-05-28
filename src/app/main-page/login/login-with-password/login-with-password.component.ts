8ce9081722041f17ac22e400177fcc50486f1eda
Alexey Li <aleksey.lee@gmail.com>
2023-05-28 15:24:04 +0200

    v2 register login

--- src/app/main-page/login/login-with-password/login-with-password.component.ts
+++ src/app/main-page/login/login-with-password/login-with-password.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { AppComponent } from 'src/app/app.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-login-with-password',
@@ -11,22 +15,34 @@ export class LoginWithPasswordComponent {
   loginMessage = 'Logged In Successfully';
 
   loginForm = this.fb.group({
-    login: ['', [Validators.required, Validators.name]],
+    username: ['', [Validators.required]],
     password: ['', [Validators.required, Validators.minLength(6)]],
   });
+handleErrorLogin: any;
 
   constructor(
     private fb: NonNullableFormBuilder,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private router: Router,
+    private authService: AuthService
   ) {}
-
-  public handleErrorLogin = (email: string, errorName: string) => {
-    return (
-      this.loginForm.get(email)?.touched &&
-      this.loginForm.get(email)?.errors &&
-      this.loginForm.get(email)?.hasError(errorName)
-    );
-  };
+  
+  login() {
+    if (!this.loginForm.valid) {
+      return;
+    }
+    this.authService.login(this.loginForm.value).pipe(
+      // route to protected/dashboard, if login was successfull
+      tap(() => this.router.navigate(['../main-page/main.page']))
+    ).subscribe();
+  }
+  // public handleErrorLogin = (email: string, errorName: string) => {
+  //   return (
+  //     this.loginForm.get(email)?.touched &&
+  //     this.loginForm.get(email)?.errors &&
+  //     this.loginForm.get(email)?.hasError(errorName)
+  //   );
+  // };
 
   openSnackBar(message: string, action: string) {
     this.snackBar.open(message, action);
