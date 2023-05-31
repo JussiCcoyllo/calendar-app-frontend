7fe4e5d8dfa7ca970aaab9d8c7b03293ebc9fa94
Alexey Li <aleksey.lee@gmail.com>
2023-05-31 19:10:19 +0200

    v3.0 with landing page and started with navigation

--- src/app/main-page/login/login.component.ts
+++ src/app/main-page/login/login.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  router: any;
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  goToLogin() {
+    this.router.navigateByUrl('/login');
+  }
 
 }
