ee3b23538bc93f61f6d5992645ed14af415812d1
Jussi Ccoyllo <46483286+JussiCcoyllo@users.noreply.github.com>
2023-06-05 09:39:31 +0200

    Update login.component.ts
    validation fixed

--- src/app/main-page/login/login.component.ts
+++ src/app/main-page/login/login.component.ts
@@ -18,7 +18,7 @@ export class LoginComponent {
 
   loginForm = new FormGroup({
     username: new FormControl(null, [Validators.required]),
-    password: new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
   },)
 
   constructor(
